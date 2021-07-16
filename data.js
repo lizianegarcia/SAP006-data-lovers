f2f6e8596755b31f2dd9d84fe7c34c1e7b121dc1
Your Name <you@example.com>
2021-07-16 14:36:53 -0300

    Updates

---
+++ data.js
@@ -0,0 +1,34 @@
+// Mostrar quantidade de Pokémons que quisermos selecionar
+export const showPokemons = (pokemonsList) => pokemonsList.slice(0, 10);
+
+// Filtrar Pokémons por nome através do input text
+export const filterByName = (pokemonsList, pokemonsName) => {
+  const lowerCaseName = pokemonsName.toLowerCase();
+  return pokemonsList
+    .filter((pokemon) => pokemon.name.toLowerCase().startsWith(lowerCaseName))
+    .slice(0, 10);
+};
+
+//Filtrar Pokémons por tipo através do select
+export const filterByType = (data, selectFilter) =>
+  data.filter((pokemon) => pokemon.type.includes(selectFilter));
+
+//Ordenar Pokémons por CP Max->Min/Min->Max
+export const orderCP = (data, selectOrder) => {
+  if (selectOrder === "MaxCPMinCP") {
+    return data.sort(
+      (maxcp, mincp) => mincp.stats["max-cp"] - maxcp.stats["max-cp"]
+    );
+  }
+  if (selectOrder === "MinCPMaxCP") {
+    return data.sort(
+      (maxcp, mincp) => maxcp.stats["max-cp"] - mincp.stats["max-cp"]
+    );
+  }
+};
+
+//Cálculo
+export const percentType = (data, filterCalc) => {
+  const calcPoke = data.filter((pokemon) => pokemon.type.includes(filterCalc));
+  return Math.round((calcPoke.length / data.length) * 100);
+};
